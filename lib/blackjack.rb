bbb1d7205f5d674542747a82064ad1417674e40c
Danny Wakeling <wakeling101@hotmail.com>
2019-03-01 19:58:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,6 +31,14 @@ end
 
 def hit?(card_total)
   prompt_user
+  input = get_user_input
+  if input == 'h'
+    card_total += deal_card
+  elsif onput == 's'
+    card_total
+  else 
+    invalid_command
+  end
 end
 
 def invalid_command
